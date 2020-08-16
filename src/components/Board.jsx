8aab80cc42296b20ad0130fa651d35f6c9f19d59
ep3824 <ethan.parent1@gmail.com>
2020-08-16 10:50:29 -0600

    refactor Piece component as a class component; get the placing of x and o to work on a blank board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,42 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Piece from './Piece'
 
 function Board() {
   const [boardState, setBoardState] = useState([
-    'X','','O',
-    '','X','',
-    'O','','X'
+    '','','',
+    '','','',
+    '','',''
   ])
 
-  let newBoardState = boardState;
-  let clickCounter = 1;
-  function handleClick(index) {
-    console.log('clicked')
-    if (clickCounter % 2 === 0) {
-      newBoardState[index] = 'X'
-    } else {
-      newBoardState[index] = 'O'
-    }
-    setBoardState(newBoardState)
-    clickCounter += 1;
+  
+  function updateState(newState) {
+    setBoardState(newState)
   }
 
   return (
   <div id="board" className="container">
     <div className="row mt-5 justify-content-center">
-      <div className="cell col-xs" ><Piece boardstate={boardState[0]} onClick={handleClick} index={0}/></div>
-      <div className="cell col-xs" ><Piece boardstate={boardState[1]} onClick={handleClick} index={1}/></div>
-      <div className="cell col-xs" ><Piece boardstate={boardState[2]} onClick={handleClick} index={2}/></div>
+      <div className="cell col-xs" ><Piece updateState={updateState} boardstate={boardState} index={0} key={0}/></div>
+      <div className="cell col-xs" ><Piece updateState={updateState} boardstate={boardState} index={1} key={1}/></div>
+      <div className="cell col-xs" ><Piece updateState={updateState} boardstate={boardState} index={2} key={2}/></div>
     </div>
     <div className="row justify-content-center">
-      <div className="cell col-xs" ><Piece boardstate={boardState[3]} onClick={handleClick} index={3}/></div>
-      <div className="cell col-xs" ><Piece boardstate={boardState[4]} onClick={handleClick} index={4}/></div>
-      <div className="cell col-xs" ><Piece boardstate={boardState[5]} onClick={handleClick} index={5}/></div>
+      <div className="cell col-xs" ><Piece updateState={updateState} boardstate={boardState} index={3} key={3}/></div>
+      <div className="cell col-xs" ><Piece updateState={updateState} boardstate={boardState} index={4} key={4}/></div>
+      <div className="cell col-xs" ><Piece updateState={updateState} boardstate={boardState} index={5} key={5}/></div>
     </div>
     <div className="row justify-content-center">
-      <div className="cell col-xs" ><Piece boardstate={boardState[6]} onClick={handleClick} index={6}/></div>
-      <div className="cell col-xs" ><Piece boardstate={boardState[7]} onClick={handleClick} index={7}/></div>
-      <div className="cell col-xs" ><Piece boardstate={boardState[8]} onClick={handleClick} index={8}/></div>
+      <div className="cell col-xs" ><Piece updateState={updateState} boardstate={boardState} index={6} key={6}/></div>
+      <div className="cell col-xs" ><Piece updateState={updateState} boardstate={boardState} index={7} key={7}/></div>
+      <div className="cell col-xs" ><Piece updateState={updateState} boardstate={boardState} index={8} key={8}/></div>
     </div>
   </div>
   )
--- src/components/Piece.jsx
+++ src/components/Piece.jsx
@@ -1,24 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import X from './X'
 import O from './O'
 
-function Piece(props) {
-  function pieceChecker(props) {
-    if (props.boardstate === 'X') {
+
+class Piece extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      board: props.boardstate,
+      clickCounter: 1,
+    }
+    this.handleClick = this.handleClick.bind(this);
+    this.pieceChecker = this.pieceChecker.bind(this);
+  }
+
+  handleClick(e) {
+    let newBoardState = this.props.boardstate;
+    console.log('clicked')
+    console.log('props boardstate', this.props.boardstate)
+    if (this.state.clickCounter % 2 === 0) {
+      newBoardState[this.props.index] = 'X'
+    } else {
+      newBoardState[this.props.index] = 'O'
+    }
+    this.setState({
+      clickCounter: this.state.clickCounter + 1
+    })
+    this.props.updateState(newBoardState)
+  }
+
+
+  pieceChecker(props) {
+    if (props.boardstate[props.index] === 'X') {
       return <X boardstate={'X'}/>
-    } else if (props.boardstate === 'O') {
+    } else if (props.boardstate[props.index] === 'O') {
       return <O boardstate={'O'}/>
     } else {
       return <O boardstate={''}/>
     }
   }
-  
 
-  return (
-  <div className="piece border">
-    {pieceChecker(props)}
-  </div>
-  )
+  render() {
+    return (
+    <div className="piece border" onClick={this.handleClick}>
+      {this.pieceChecker(this.props)}
+      {/* {this.props.boardstate[0] === 'O' ? <O boardstate={'O'} /> : <X boardstate={'X'}/>} */}
+    </div>
+    )
+  }
 }
 
 export default Piece;
